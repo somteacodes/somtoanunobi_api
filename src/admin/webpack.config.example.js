3511cae1a2162bcec21d8eb449ebcf21287888ac
Somtochukwu Ifeanyi Anunobi <a025073m@student.staffs.ac.uk>
2023-08-13 23:53:06 +0100

    added HTML wysiwyg editor

--- src/admin/webpack.config.example.js
+++ src/admin/webpack.config.example.js
@@ -5,5 +5,10 @@ module.exports = (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
+  config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+    /^tippy\.js$/,
+    'tippy.js/dist/tippy-bundle.umd.min.js'
+  ))
+
   return config;
 };
